5ffa32de2b0ed511b89cd0c244c5e8a5f58c6946
yassir elyasini <elyasiniyassir40@gmail.com>
2024-04-28 16:32:52 +0100

    setup mysql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,6 @@
---script that prepares a MySQL server for the project--
-CREATE DATABASE if not exists hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'local' identified by 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON 'hbnb_dev_db'.* to 'hbnb_dev'@'local';
-Grant select ON 'performance_schema'.* to 'hbnb_dev'@'local';
+--script that prepares a MySQL server for the project
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON 'hbnb_dev_db'.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON 'performance_schema'.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
