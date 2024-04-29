00a2857627d5787efdb8c938ad711123ff558933
yassir elyasini <elyasiniyassir40@gmail.com>
2024-04-29 20:09:58 +0100

    upgrade all function

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return self.__objects
+
+        obj = {}
+        for key in self.__objects:
+            if str(cls.__name__) is key:
+                obj[key] = self.__objects[key]
+        return obj
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -60,7 +67,7 @@ class FileStorage:
         obj_key = f"{obj.__class__.__name__}.{obj.id}"
         
         try:
-            del self.__objects[obj_Key]
+            del self.__objects[obj_key]
         except KeyError:
             return
 
