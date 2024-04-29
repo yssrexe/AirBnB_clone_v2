3601bf5400ee5c7b754b68bb14d40a8068aff205
yassir elyasini <elyasiniyassir40@gmail.com>
2024-04-29 21:12:10 +0100

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,19 +2,31 @@
 """This module defines a class to manage file storage for hbnb clone"""
 import json
 
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
+    classes = {
+                'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                'State': State, 'City': City, 'Amenity': Amenity,
+                'Review': Review
+              }
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        if cls is None:
+        if not cls and cls not in self.classes.values():
             return self.__objects
 
         obj = {}
-        for key in self.__objects:
+        for key in self.__objects.keys():
             if str(cls.__name__) is key:
                 obj[key] = self.__objects[key]
         return obj
