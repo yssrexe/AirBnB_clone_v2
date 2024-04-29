b9e69a08ef78736a75b2fd6894a41e786dae7b30
yassir elyasini <elyasiniyassir40@gmail.com>
2024-04-29 21:20:08 +0100

    ok

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -22,12 +22,12 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        if not cls and cls not in self.classes.values():
+        if not cls or cls not in self.classes.values():
             return self.__objects
 
         obj = {}
         for key in self.__objects.keys():
-            if str(cls.__name__) is key:
+            if str(cls.__name__) in key:
                 obj[key] = self.__objects[key]
         return obj
 
@@ -46,33 +46,20 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                    self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = self.classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
-        """delete obj from __objects"""
-        if obj :
-            obj_key = f"{obj.__class__.__name__}.{obj.id}"
+        """Delete an object from __objects"""
+        if obj:
+            key = f"{obj.__class__.__name__}.{obj.id}"
             try:
-                del self.__objects[obj_key]
+                del self.__objects[key]
             except KeyError:
                 pass
