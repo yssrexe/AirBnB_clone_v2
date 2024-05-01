80c28c10ddff9cd9a99950b29d0d62cb11de4c8c
probrahim <ibrahimhassani674@gmail.com>
2024-05-01 13:48:44 +0200

    task 07

--- models/user.py
+++ models/user.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 
-class User(BaseModel):
+class User(BaseModel,Base):
+
+    """ make table "users"""
+
+    __tablename__ = 'users'
+
+
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
