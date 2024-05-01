0ad0953f5b830a68e7c315bf7ed7b4811f49048a
probrahim <ibrahimhassani674@gmail.com>
2024-05-01 14:05:58 +0200

    task 07

--- models/user.py
+++ models/user.py
@@ -14,3 +14,5 @@ class User(BaseModel,Base):
     """This class defines a user by various attributes"""
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
