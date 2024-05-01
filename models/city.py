f9f1ea92acfc2607744d1e5354ddcc71aefaff9f
yassir elyasini <elyasiniyassir40@gmail.com>
2024-05-01 19:34:16 +0100

    hello

--- models/city.py
+++ models/city.py
@@ -10,6 +10,6 @@ Base = declarative_base()
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    __tablename__ = "cities"
+    __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"))
--- models/state.py
+++ models/state.py
@@ -12,7 +12,7 @@ Base = declarative_base()
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
+    __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="delete")
     if getenv("HBNB_TYPE_STORAGE") != "db":
