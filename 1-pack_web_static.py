e6f91cd51e41f9b3f0e56854089c3afa896d1822
yssrexe <elyasiniyassir40@gmail.com>
2024-05-15 21:48:52 +0100

    task 01

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,6 +3,7 @@
  Fabric script that generates a .tgz
 """
 
+
 def do_pack():
     """
     files in the folder web_static
@@ -10,7 +11,7 @@ def do_pack():
     from fabric.api import local
     from datetime import datetime
     from os.path import isdir
-    
+
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
         if isdir("versions") is False:
@@ -18,5 +19,5 @@ def do_pack():
         file_name = "versions/web_static_{}.tgz".format(date)
         local("tar -cvzf {} web_static".format(file_name))
         return file_name
-    except:
+    except ValueError:
         return None
