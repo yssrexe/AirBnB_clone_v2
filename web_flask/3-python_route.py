f62db0e50c72f931fe2b6b77de1447b0bb25c4f6
yssrexe <elyasiniyassir40@gmail.com>
2024-05-30 11:11:41 +0100

    task 03

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def fst_page():
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def sc_page():
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def tr_page(text : str):
+    return "C {}".format(text.replace('_', ' '))
+
+@app.route('/python/', defaults={'text':'is cool'} ,strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def py_page(text : str):
+    return f"Python {text.replace('_', ' ')}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
