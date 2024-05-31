642d7e6aacd0412a983dd494f3a5fffffb2ca426
yssrexe <elyasiniyassir40@gmail.com>
2024-05-31 18:42:11 +0100

    task 07

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -64,3 +64,7 @@ class FileStorage:
                 del self.__objects[key]
             except KeyError:
                 pass
+
+    def close(self):
+        """method for deserializing the JSON file to objects"""
+        self.reload()
