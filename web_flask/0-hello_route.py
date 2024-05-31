1691b92739f12b580400a0b6d7d30e35049a5928
yssrexe <elyasiniyassir40@gmail.com>
2024-05-31 08:15:17 +0100

    py style

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+"""hello_route"""
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """This route returns a "Hello HBNB!" string"""
     return "Hello HBNB!"
 
 
