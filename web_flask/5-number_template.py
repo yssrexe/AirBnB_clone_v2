161986d59b75eeb8074768ea15bab52a1506ee57
yssrexe <elyasiniyassir40@gmail.com>
2024-05-31 09:04:24 +0100

    task 6

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -38,7 +38,7 @@ def num_page(n: str):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def display_num(n):
-    """This route returns a "number" string"""
+    """This route returns a n string"""
     return render_template('5-number.html', n=n)
 
 
