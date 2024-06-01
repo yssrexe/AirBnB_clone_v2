37859f23edf41c3b34994280b99209fb6a317c26
yssrexe <elyasiniyassir40@gmail.com>
2024-06-01 12:31:23 +0100

    rask 99

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""hello_route"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -8,11 +9,13 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def close_app(exception=None):
+    """Closes the storage session"""
     storage.close()
 
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
+    """Closes the storage session"""
     states = storage.all(State).values()
     return render_template('8-cities_by_states.html', states=states)
 
