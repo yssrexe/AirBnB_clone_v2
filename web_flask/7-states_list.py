8a650f8bf16fe23bf639965ed642d47f314d48ae
yssrexe <elyasiniyassir40@gmail.com>
2024-06-01 12:41:24 +0100

    rask 88888888888888

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
-"""hello_route"""
+"""
+Flask application to list States
+"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 
-# storage.all(...)
-
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown_storage(exception=None):
-    """Closes the storage session"""
+def close_session(request):
+    """
+    Close session after each request
+    """
     storage.close()
 
 
-@app.route("/states_list",  strict_slashes=False)
-def states_list():
-    """shows an html page with a list of states"""
-
+@app.route('/states_list')
+def states():
+    """
+    List all states
+    """
     states = [state for state in storage.all(State).values()]
-    return render_template("7-states_list.html", states=states)
+    return render_template('7-states_list.html', states=states)
 
 
 if __name__ == '__main__':
