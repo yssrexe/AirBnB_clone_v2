9459950081dfbc9d1552d337befa32fa03230138
yssrexe <elyasiniyassir40@gmail.com>
2024-06-02 17:30:58 +0100

    task 100000000000000000000000000

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
