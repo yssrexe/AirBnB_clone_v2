900ac74888e153b5118ba2c8b4ca141aa8be4bae
yssrexe <elyasiniyassir40@gmail.com>
2024-06-02 11:35:32 +0100

    getenvyy

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""hello route"""
+"""hello_route"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -9,16 +9,16 @@ app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def close_storage(exception=None):
-    """close storage"""
+def teardown_storage(exception=None):
+    """Closes the storage session"""
     storage.close()
 
 
-@app.route('/states', strict_slashes=False)
-def ret_States():
-    """return states"""
-    states = storage.all(State).values()
-    return render_template('9-states.html', states=states)
+@app.route("/states", strict_slashes=False)
+def states():
+    """fetches all states and presents them on html page"""
+    allstates = storage.all(State).values()
+    return render_template("9-states.html", states=allstates)
 
 
 @app.route("/states/<id>", strict_slashes=False)
