95d11630e1f94e433b0cc31e908aaf56779e3130
yssrexe <elyasiniyassir40@gmail.com>
2024-06-02 11:16:13 +0100

    task 10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -4,17 +4,21 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 
+
 app = Flask(__name__)
+
 
 @app.teardown_appcontext
 def close_storage(exception=None):
     """close storage"""
     storage.close()
 
+
 @app.route('/states', strict_slashes=False)
 def ret_States():
     states = storage.all(State).values()
     return render_template('9-states.html', states=states)
+
 
 @app.route("/states/<id>", strict_slashes=False)
 def state_id(id):
@@ -27,5 +31,5 @@ def state_id(id):
     return render_template("9-states.html", state=state)
 
 
-if __name__ =='__main__':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
