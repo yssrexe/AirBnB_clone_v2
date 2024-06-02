16ca3a4b111129a8b369922c7163745c817c8c70
yssrexe <elyasiniyassir40@gmail.com>
2024-06-02 14:36:42 +0100

    task 11

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""hello world"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def storage_close(error):
+    """storage close"""
+    storage.close()
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    states = storage.all(State)
+    amenities = storage.all(Amenity)
+    return render_template('10-hbnb_filters.html', states=states, amenities=amenities)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
